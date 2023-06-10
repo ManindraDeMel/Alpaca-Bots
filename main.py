74b819f920eca212704f9142d11bc6462fe30a5a
Manindra de Mel <u7156805@anu.edu.au>
2023-06-11 02:24:41 +1000

    Updated for new model

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ import pytz
 load_dotenv()
 
 SUBSCRIPTION = False
-HISTORICAL_DATA = 7 # days for data retrieval 
+SHORT_PERIOD = 1 # day/s
+LONG_PERIOD = 5 # day/s
+HISTORICAL_DATA = 4 # weeks for data retrieval 
 API_KEY = os.getenv("APCA-API-KEY-ID")
 API_SECRET_KEY = os.getenv("APCA-API-SECRET-KEY")
 BASE_URL = 'https://data.alpaca.markets/v2'
 ALPACA_ORDERS_URL = 'https://paper-api.alpaca.markets/v1/orders'
+ALPACA_POSITIONS_URL = 'https://paper-api.alpaca.markets/v1/positions'
+ALPACA_ACCOUNT_URL = 'https://paper-api.alpaca.markets/v1/account'
 
 headers = {
     "APCA-API-KEY-ID": API_KEY,
@@ -20,6 +24,24 @@ headers = {
 }
 
 stock_list = ['MSFT', 'AMZN', 'TSLA', 'GOOGL', 'AAPL']
+
+def get_positions():
+    try:
+        response = requests.get(ALPACA_POSITIONS_URL, headers=headers)
+        positions = response.json()
+        return {position['symbol']: position for position in positions}
+    except Exception as e:
+        print(f"Error getting positions: {str(e)}")
+        return {}
+
+def get_account():
+    try:
+        response = requests.get(ALPACA_ACCOUNT_URL, headers=headers)
+        account_info = response.json()
+        return account_info
+    except Exception as e:
+        print(f"Error getting account information: {str(e)}")
+        return None
 
 def get_bars(stock):
     try:
@@ -30,13 +52,13 @@ def get_bars(stock):
         else:
             end_time = utc_now - timedelta(minutes=16)
 
-        start_time = end_time - timedelta(days=HISTORICAL_DATA) # adjust as needed
+        start_time = end_time - timedelta(weeks=HISTORICAL_DATA) # adjust as needed
 
         start = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
         end = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
         response = requests.get(f'{BASE_URL}/stocks/{stock}/bars', headers=headers, 
-                                params={"timeframe": '1W', 'start': start, 'end': end})
+                                params={"timeframe": '1D', 'start': start, 'end': end})
         data = response.json()
         if 'bars' in data:
             bars_data = data['bars']
@@ -50,7 +72,7 @@ def get_bars(stock):
         print(str(e))
         return []
 
-def get_moving_averages(stock, short_period=5, long_period=20):
+def get_moving_averages(stock, short_period=SHORT_PERIOD, long_period=LONG_PERIOD):
     bars = get_bars(stock)
     if len(bars) < long_period:
         return None, None
@@ -63,43 +85,63 @@ def get_moving_averages(stock, short_period=5, long_period=20):
 
     return short_ma, long_ma
 
-def place_order(stock, capital):
-    order = {
-        "symbol": stock,
-        "qty": capital,
-        "side": "buy",
-        "type": "market",
-        "time_in_force": "gtc"
-    }
-    r = requests.post(ALPACA_ORDERS_URL, headers=headers, json=order)
-    print(f"Placed order: {json.dumps(r.json(), indent=2)}")
+def place_order(stock, qty):
+    try:
+        order = {
+            "symbol": stock,
+            "qty": qty,
+            "side": "buy",
+            "type": "market",
+            "time_in_force": "gtc"
+        }
+        r = requests.post(ALPACA_ORDERS_URL, headers=headers, json=order)
+        print(f"Placed order: {json.dumps(r.json(), indent=2)}")
+    except Exception as e:
+        print(f"Error placing order: {str(e)}")
 
 def sell_order(stock, qty):
-    order = {
-        "symbol": stock,
-        "qty": qty,
-        "side": "sell",
-        "type": "market",
-        "time_in_force": "gtc"
-    }
-    r = requests.post(ALPACA_ORDERS_URL, headers=headers, json=order)
-    print(f"Sold order: {json.dumps(r.json(), indent=2)}")
+    try:
+        order = {
+            "symbol": stock,
+            "qty": qty,
+            "side": "sell",
+            "type": "market",
+            "time_in_force": "gtc"
+        }
+        r = requests.post(ALPACA_ORDERS_URL, headers=headers, json=order)
+        print(f"Sold order: {json.dumps(r.json(), indent=2)}")
+    except Exception as e:
+        print(f"Error selling order: {str(e)}")
+
+def trading_time():
+    now = datetime.now(pytz.timezone('America/New_York'))
+    open_time = datetime(now.year, now.month, now.day, hour=9, minute=30, tzinfo=pytz.timezone('America/New_York'))
+    close_time = datetime(now.year, now.month, now.day, hour=16, tzinfo=pytz.timezone('America/New_York'))
+    return open_time <= now <= close_time
 
 def main():
     while True:
-        for stock in stock_list:
-            short_ma, long_ma = get_moving_averages(stock)
-            if short_ma is None:
-                continue
-            capital = 100  # adjust this to your capital
-            if short_ma > long_ma:
-                place_order(stock, capital)
-                print(f"Buying {stock}")
-            elif short_ma < long_ma:
-                sell_order(stock, capital)
-                print(f"Selling {stock}")
-            else:
-                print(f"Holding position for stock: {stock}")
+        if trading_time():
+            account_info = get_account()
+            if account_info is not None:
+                capital = min(100, float(account_info['cash']))  # adjust this to your capital
+                positions = get_positions()
+
+                for stock in stock_list:
+                    short_ma, long_ma = get_moving_averages(stock)
+                    if short_ma is None:
+                        continue
+
+                    if short_ma > long_ma:
+                        place_order(stock, capital)
+                        print(f"Buying {stock}")
+                    elif short_ma < long_ma and stock in positions:
+                        sell_order(stock, capital)
+                        print(f"Selling {stock}")
+                    else:
+                        print(f"Holding position for stock: {stock}")
+        else:
+            print("Market is closed. Waiting for it to open...")
         time.sleep(60)
 
 if __name__ == "__main__":
